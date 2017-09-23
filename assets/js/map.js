731a3634c1e8bccd87b145d2cfe6593cd95b1255
Ruel Mindo <wrelmind@gmail.com>
2017-09-24 04:01:26 +0800

    New

--- assets/js/map.js
+++ assets/js/map.js
@@ -8,133 +8,50 @@ function init() {
 
 	    if( this.readyState == 4 && this.status == 200 ) {
 
-			var marker, item, res = JSON.parse( this.responseText );
+			var resto = JSON.parse( this.responseText );
 
-			var infobox  = new google.maps.InfoWindow();
-	        var location = new google.maps.LatLng( res[0]['lat'], res[0]['lng'] );
-
-
-			var map = new google.maps.Map( document.getElementById('map'), {
-				zoom: 16,
-				center: location,
+			var googlemap = new google.maps.Map( document.getElementById('map'), {
+				zoom: 15,
+				center: new google.maps.LatLng( resto[0]['lat'], resto[0]['lng'] ),
 				mapTypeId: google.maps.MapTypeId.terrain
 			});
 
-
-			var circle = new google.maps.Circle({
-				map: 			map,
-				strokeWeight: 	1,
-				fillOpacity: 	0.1,
-				strokeOpacity: 	0.5,
-				radius: 		1000,
-				editable: 		true,
-				draggable: 		true,
-				fillColor: 		'#ff0000',
-				strokeColor: 	'#ff0000',
-				center: 		location,
-			});
-
-
-
-
-
-
-
-	    	var items = '';
-
-			for( i = 0; i < res.length; i++ ) {
-
-				var markerLocation = new google.maps.LatLng( res[i]['lat'], res[i]['lng'] );
-
-
-				marker = new google.maps.Marker({
-
-					map: map,
-					icon: new google.maps.MarkerImage( 'assets/images/icon.png', new google.maps.Size(32, 32) ),
-					position: new google.maps.LatLng( res[i]['lat'], res[i]['lng'] )
-
-				});
-
-
-
-				if( res[i]['name'] ) {
-
-
-					item = '<h3>' + res[i]['name'] + '</h3>'
-							+'<div class="meta">'
-							+'<i class="rate star-'+ res[i]['star'] +'"></i>'
-							+'<p><b>Visits: </b> '+ res[i]['visits'] +'/day</p>'
-							+'<p><b>Patrons: </b> '+ res[i]['patrons'] +'</p>'
-							+'<p><b>Specialty: </b> '+ res[i]['specialty'] +'</p>'
-							+'</div>';
-
-
-					google.maps.event.addListener( marker, 'click', ( function( marker, item, pos ) {
-
-
-						return function() {
-
-							var tip = '<div class="tip">'+ item + '<button id="getlocation">Get Direction</button></div>';
-
-
-							infobox.setContent( tip );
-							infobox.open( map, marker );
-
-							getLocation( pos );
-						};
-
-
-					})( marker, item, [res[i]['lat'], res[i]['lng']] ));
-
-
-
-
-
-
-
-					google.maps.event.addListener( circle, 'dragend', (function( restaurant ) {
-
-
-
-						return function() {
-
-							var markers = [];
-							var diff = google.maps.geometry.spherical.computeDistanceBetween( this.getCenter(), markerLocation );
-
-
-							if( this.getBounds().contains( markerLocation ) && diff <= this.getRadius() ) {
-
-							    markers.push( restaurant );
-							}
-
-							console.log( markers.length );
-						};
-
-
-
-					})( res[i]['name'] )); 
-
-
-
-
-
-					items += '<div class="item type-'+ res[i]['type'] +'">'+ item +'</div>';
-				}
-			}
-
-
-
-
-			loadPanel( map, items );
+			loadRestaurants( googlemap, resto );
 		}
 
 	};
-
 
 	http.open( 'GET', 'restaurants.json', true );
 
 	http.send();
 }
+
+
+
+
+
+function infoBox( marker, infobox, item, location ) {
+
+
+	google.maps.event.addListener( marker, 'click', ( function( marker, infobox, item, location ) {
+
+
+		return function() {
+
+			var tip = '<div class="tip">'+ item + '<button id="getlocation">Get Direction</button></div>';
+
+
+			infobox.setContent( tip );
+			infobox.open( map, marker );
+
+			getLocation( location );
+		};
+
+
+	})( marker, infobox, item, location ));
+
+}
+
 
 
 
@@ -165,7 +82,7 @@ function loadPanel( map, items ) {
 		document.getElementsByTagName('input')[input].onchange = function() {
 
 
-			var list = document.getElementsByClassName( 'type-' + this.value );
+			var list = document.getElementsByClassName( 'type-' + this.id );
 
 
 			for( i = 0; i < list.length; i++ ) {
@@ -185,6 +102,28 @@ function loadPanel( map, items ) {
 
 }
 
+
+
+
+
+function moveCircle( circle, markerPosition ) {
+
+
+	google.maps.event.addListener( circle, 'center_changed', (function() {
+
+
+		return function() {
+
+			var diff = google.maps.geometry.spherical.computeDistanceBetween( this.getCenter(), markerPosition );
+
+
+			if( this.getBounds().contains( markerPosition ) && diff <= this.getRadius() ) {
+
+			}
+		};
+
+	})); 
+}
 
 
 
@@ -209,6 +148,7 @@ function getLocation( pos ) {
 					center: {lat: position.coords.latitude, lng: position.coords.longitude },
 					mapTypeId: google.maps.MapTypeId.terrain 
 				});
+
 
 				directionsDisplay.setMap( mapdirection );
 
@@ -241,6 +181,66 @@ function getLocation( pos ) {
 	    }
 
 	}
+}
 
 
+
+
+
+
+function loadRestaurants( googlemap, resto ) {
+
+
+	var item, marker, items = '';
+
+
+	var infobox = new google.maps.InfoWindow();
+	var circle  = new google.maps.Circle({
+		map: 			googlemap,
+		strokeWeight: 	1,
+		fillOpacity: 	0.1,
+		strokeOpacity: 	0.5,
+		radius: 		1000,
+		editable: 		true,
+		draggable: 		true,
+		fillColor: 		'#ff0000',
+		strokeColor: 	'#ff0000',
+		center: 		new google.maps.LatLng( resto[0]['lat'], resto[0]['lng'] ),
+	});
+
+
+
+
+	for( i = 0; i < resto.length; i++ ) {
+
+		var position = [resto[i]['lat'], resto[i]['lng']];
+
+		var markerPosition = new google.maps.LatLng( position[0], position[1] );
+
+
+		marker = new google.maps.Marker({
+			map: googlemap,
+			icon: new google.maps.MarkerImage( 'assets/images/icon.png', new google.maps.Size(32, 32) ),
+			position: markerPosition
+		});
+
+
+		item = '<h3><i class="color"></i>' + resto[i]['name'] + '</h3>'
+				+'<div class="meta">'
+				+'<i class="rate star-'+ resto[i]['star'] +'"></i>'
+				+'<p><b>Visits: </b> '+ resto[i]['visits'] +'/day</p>'
+				+'<p><b>Patrons: </b> '+ resto[i]['patrons'] +'</p>'
+				+'<p><b>Specialty: </b> '+ resto[i]['specialty'] +'</p>'
+				+'</div>';
+
+		items += '<div class="item type-'+ resto[i]['type'] +'">'+ item +'</div>';
+
+
+
+		infoBox( marker, infobox, item, position );
+
+		moveCircle( circle, markerPosition );
+	}
+
+	loadPanel( googlemap, items );
 }
